9c55beeff518f7561d2110e4a85dcbefcb50ef7c
Mikael-UofA <lnineza@ualberta.ca>
2025-01-02 21:36:33 +0300

    Added the map

--- DisplayMaps/script.js
+++ DisplayMaps/script.js
@@ -1,6 +1,20 @@
 'use strict'
 
+// Initialize map and set its view
+var map = L.map('map').setView([39.8283, -98.5795], 5);
+const geojsonPath = '../PrepareData/sources/usable.geojson';
+const colorMapping = {
+    '1': 'blue',
+    '-1': 'red', 
+    '0': 'purple'
+};
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Add tile layer
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
 document.addEventListener('DOMContentLoaded', () => {
+    loadGeoJSON();
     const aboutLink = document.querySelector('.about-popup')
     const currentCourtType = document.querySelector('.court-display');
     const currentModeType = document.querySelector('.mode-display');
@@ -56,3 +70,39 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 });
+
+// Function to style features based on PARTISANSHIP property
+function getColor(partisanship) {
+    return partisanship === 1 ? 'blue' : partisanship === -1 ? 'red' : 'purple';
+}
+// Function to create a popup for each feature
+function onEachFeature(feature, layer) {
+    var content = `<h3>${feature.properties.NAME}</h3>
+                   <p>FID: ${feature.properties.FID}</p>
+                   <p>CHIEF JUDGE: ${feature.properties.CHIEF_JUDGE}</p>
+                   <p>ACTIVE JUDGES: ${feature.properties.ACTIVE_JUDGES}</p>
+                   <p>SENIOR ELIGIBLE JUDGES: ${feature.properties.SENIOR_ELIGIBLE_JUDGES}</p>`;
+    layer.bindPopup(content);
+}
+
+function loadGeoJSON() {
+    fetch('../PrepareData/sources/usable.geojson')
+        .then(response => response.json())
+        .then(data => {
+            L.geoJSON(data, {
+                style: function (feature) {
+                    return {
+                        fillColor: colorMapping[feature.properties.PARTISANSHIP] || '#FFFFFF',
+                        weight: 2,
+                        color: '#000000',
+                        opacity: 1,                   
+                        fillOpacity: 0.5
+                    };
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        })
+        .catch(error => {
+            console.error("Error loading GeoJSON data:", error);
+        });
+}
